13ce75754e5b593d3f44435c1fbb00f9c8ea3ec2
vinciliba <39335992+vinciliba@users.noreply.github.com>
2020-06-03 15:40:58 +0200

    1st update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import { CardList } from '../src/components/card-list/card-list.component';
+import  { SearchBox }  from '../src/components/search-box/search-box.componet';
 
 function App() {
+
+  // const [monsters,setMonsters] =useState([{name:'Frankestein',id:'1'},{name:'Deacula',id:'2'},{name:'Zombie',id:'3'}]);
+   
+  const [monsters,setMonsters] = useState([]);
+  const [searchField,setSearchField] = useState(null);
+  
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then (response => response.json())
+    .then (users => setMonsters(users))
+  });
+
+const handleChange = (e) => {
+  setSearchField(e.target.value) 
+};
+
+  const filterMonsters = monsters.filter(monster => monster.name.includes(searchField));
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <SearchBox placeholder={'search monster'} handleChange={handleChange} />
+     <CardList monsters={filterMonsters} />
     </div>
+
   );
 }
 
 export default App;
+
